8397a543496d8bb363ff4330348913d02a14470d
gagan <gagantiwari1299@gmail.com>
2020-11-03 11:46:04 +0530

    made header and footer

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
+import 'font-awesome/css/font-awesome.css';
+import 'bootstrap-social/bootstrap-social.css'
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
